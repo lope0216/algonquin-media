e0fe456c5ea96482b7feeb1b8398db04b4447eb7
Safae Moudkar <moud0008@algonquinlive.com>
2024-12-02 10:45:56 -0500

    all footers centred and updated

--- pages/addAlbum.php
+++ pages/addAlbum.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= dirname($_SERVER['PHP_SELF']) . '/../public/css/global.css' ?>" />
+    <link rel="stylesheet" href="<?= dirname($_SERVER['PHP_SELF']) . '/../public/css/addAlbum.css' ?>" />
     <title>Algonquin Media</title>
 </head>
 
